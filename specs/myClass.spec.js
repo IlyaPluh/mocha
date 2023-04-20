f29e119a3af6f6fa77ed26215976c8edbc89ebd9
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-04-20 21:21:13 +0300

    sinon spy

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,4 +1,5 @@
 var MyClass = require("../src/myClass.js");
+var sinon = require("sinon");
 var myObj = new MyClass () ;
 var chai = require("chai");
 var expect = chai.expect;
@@ -6,5 +7,15 @@ var expect = chai.expect;
 describe("Test suit", function() {
     it ("Test the add method", function() {
         expect (myObj.add(1, 2)).to.be.equal(3);
-        });
-    });
+        })
+
+    it ("spy the add method", function() {
+        var spy = sinon.spy (myObj, "add");
+        var arg1 = 10,
+        arg2 = 20; 
+        myObj.callAnotherFn(arg1, arg2); 
+        //sinon.assert. calledTwice (spy) ;
+        expect (spy.calledOnce).to.be.true;
+        expect (spy.calledWith(arg1, arg2)).to.be.true;
+    })
+})
--- src/myClass.js
+++ src/myClass.js
@@ -7,5 +7,12 @@ class MyClass {
         result = arg1 + arg2;
         return result;
     }
+
+    callAnotherFn(arg1, arg2) {
+        var result = this. add (arg1, arg2)
+        return result
+    }
+
 }
+
 module.exports = MyClass;
