00e071402d13ba0c7505b60e1f859871249beda9
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-04-22 17:58:28 +0300

    callback

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,21 +1,36 @@
 var MyClass = require("../src/myClass.js");
 var sinon = require("sinon");
-var myObj = new MyClass () ;
+var myObj = new MyClass();
 var chai = require("chai");
 var expect = chai.expect;
 
-describe("Test suit", function() {
-    it ("Test the add method", function() {
-        expect (myObj.add(1, 2)).to.be.equal(3);
-        })
-
-    it ("spy the add method", function() {
-        var spy = sinon.spy (myObj, "add");
-        var arg1 = 10,
-        arg2 = 20; 
-        myObj.callAnotherFn(arg1, arg2); 
-        //sinon.assert. calledTwice (spy) ;
-        expect (spy.calledOnce).to.be.true;
-        expect (spy.calledWith(arg1, arg2)).to.be.true;
+describe("Test suit", function () {
+    it("Test the add method", function () {
+        expect(myObj.add(1, 2)).to.be.equal(3);
     })
+
+    it("spy the add method", function () {
+        var spy = sinon.spy(myObj, "add");
+        var arg1 = 10, arg2 = 20;
+        myObj.callAnotherFn(arg1, arg2);
+        //sinon.assert.calledTwice(spy);
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWith(arg1, arg2)).to.be.true;
+    })
+
+    it("spy the callabck method", function () {
+        var callback = sinon.spy();
+        myObj.callTheCallback(callback);
+        expect(callback.calledOnce).to.be.true;
+    })
+
+    it("mock the sayHello method", function () {
+        var mock = sinon.mock(myObj);
+        var expetcation = mock.expects("sayHello");
+        expetcation.exactly(1);
+        expetcation.withArgs("Hello");
+        myObj.callAnotherFn(10, 20);
+        mock.verify()
+    })
+
 })
--- src/myClass.js
+++ src/myClass.js
@@ -1,15 +1,26 @@
 class MyClass {
-    constructor () {
-    console.log("initiate");
+
+    constructor() {
+        console.log("initiate");
     }
-    add (arg1, arg2) {
-        var result;
+
+    sayHello(str) {
+        console.log(str)
+    }
+
+    add(arg1, arg2) {
+        let result;
         result = arg1 + arg2;
         return result;
     }
 
+    callTheCallback(callback) {
+        callback();
+    }
+
     callAnotherFn(arg1, arg2) {
-        var result = this. add (arg1, arg2)
+        this.sayHello('Hello')
+        var result = this.add(arg1, arg2)
         return result
     }
 
