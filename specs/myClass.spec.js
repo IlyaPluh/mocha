b401c7d5650c3fd6fd310e25a6b3364fa7983f4f
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-04-24 17:09:20 +0300

    stub

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,17 +1,17 @@
-var MyClass = require("../src/myClass.js");
-var sinon = require("sinon");
-var myObj = new MyClass();
-var chai = require("chai");
-var expect = chai.expect;
+let MyClass = require("../src/myClass.js");
+let sinon = require("sinon");
+let myObj = new MyClass();
+let chai = require("chai");
+let expect = chai.expect;
 
-describe("Test suit", function () {
+describe.skip("Test suit", function () {
     it("Test the add method", function () {
         expect(myObj.add(1, 2)).to.be.equal(3);
     })
 
     it("spy the add method", function () {
-        var spy = sinon.spy(myObj, "add");
-        var arg1 = 10, arg2 = 20;
+        let spy = sinon.spy(myObj, "add");
+        let arg1 = 10, arg2 = 20;
         myObj.callAnotherFn(arg1, arg2);
         //sinon.assert.calledTwice(spy);
         expect(spy.calledOnce).to.be.true;
@@ -19,18 +19,28 @@ describe("Test suit", function () {
     })
 
     it("spy the callabck method", function () {
-        var callback = sinon.spy();
+        let callback = sinon.spy();
         myObj.callTheCallback(callback);
         expect(callback.calledOnce).to.be.true;
     })
 
     it("mock the sayHello method", function () {
-        var mock = sinon.mock(myObj);
-        var expetcation = mock.expects("sayHello");
+        let mock = sinon.mock(myObj);
+        let expetcation = mock.expects("sayHello");
         expetcation.exactly(1);
         expetcation.withArgs("Hello");
         myObj.callAnotherFn(10, 20);
         mock.verify()
     })
+})
 
+describe("Test suit for stub", function() {
+    it("Stub the add method", function () {
+    let stub = sinon.stub(myObj, "add");
+    stub.withArgs(10, 20)
+    .onFirstCall().returns(100)
+    .onSecondCall().returns(200);
+    expect(myObj.callAnotherFn(10, 20)).to.be.equal(100);
+    expect(myObj.callAnotherFn(10, 20)).to.be.equal(200);
+    })
 })
